68635193f0fcfe4d84882ccd108b6f9242c6d3a2
Arbaz Khan <arbazkhan772001@gmail.com>
2025-06-11 04:37:44 +0530

    <feat>(StockTrendPredictor):Deployed Version 2.2

---
+++ StockTrendPredictor/app/project_info.py
@@ -0,0 +1,8 @@
+project_name = 'stock trend predictor'
+latest_version = 2.2
+latest_version_description = 'feature - updated streamlit structure for better look n feel and optimized code.'
+version_history = {
+    1.0: 'initial version - single stock trend predictor static webpage', 
+    2.0: 'multi-stock trend predictor with cloud DBMS & daily update function',
+    2.1: 'bug - fixed mode logic;feature - added refresh button, exchange dropdown'
+}
--- StockTrendPredictor/app/streamlit_app.py
+++ StockTrendPredictor/app/streamlit_app.py
@@ -1,22 +1,4 @@
 # Import libraries
-from datetime import datetime
-from db_manager import connect_db, update_model_meta
-from dotenv import load_dotenv
-from io import BytesIO
-from pydrive2.auth import GoogleAuth
-from pydrive2.drive import GoogleDrive
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
-from sklearn.multioutput import MultiOutputRegressor
-from data_processor import collect_data, engineer_features, create_targets
-from db_manager import (connect_db, create_core_table, check_stock_in_list, 
-    add_stock_to_list, fetch_symbols, create_table_name, create_processed_table, 
-    insert_processed_data, get_latest_date, delete_old_data, update_table, 
-    update_inference_result, fetch_inference_result, update_model_meta)
-from model_trainer import train_data, upload_model_object_to_drive
-from inference import generate_trend_action, run_inference
-from stock_data_updator import data_pipeline, daily_update
 import gdown
 import joblib
 import logging
@@ -32,52 +14,52 @@ import streamlit as st
 import tempfile
 import yfinance as yf
 import warnings
-warnings.filterwarnings('ignore')
+from datetime import datetime
+from dotenv import load_dotenv
+from io import BytesIO
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputRegressor
 
-# ------------------------------------------------
-
-# Project info
-if True:
-    project_name = 'stock trend predictor'
-    latest_version = 2.1
-    latest_version_description = 'bug - fixed mode logic;feature - added refresh button, exchange dropdown'
-    version_history = {
-        1.0: 'initial version - single stock trend predictor static webpage', 
-        2.0: 'multi-stock trend predictor with cloud DBMS & daily update function'
-    }
-
-# ------------------------------------------------
+# Import project modules
+from data_processor import collect_data, engineer_features, create_targets
+from db_manager import (connect_db, create_core_table, check_stock_in_list, 
+    add_stock_to_list, fetch_symbols, create_table_name, create_processed_table, 
+    insert_processed_data, get_latest_date, delete_old_data, update_table, 
+    update_inference_result, fetch_inference_result, update_model_meta)
+from model_trainer import train_data, upload_model_object_to_drive
+from inference import generate_trend_action, run_inference
+from stock_data_updator import data_pipeline, daily_update
+from project_info import latest_version
 
 # App configuration
+warnings.filterwarnings('ignore')
 st.set_page_config(page_title='ProInvest', layout='centered')
-
-# Title Section
-st.title("ProInvest — ahead of world!")
-st.caption("Your stock assistant for optimizing investment strategies.")
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Params
-mode = int(st.query_params.get("mode", 0))
+# Initialize Session State  
+st.session_state.setdefault('symbol', '')
+st.session_state.setdefault('retry_count', -1)
+MAX_RETRIES = 10 
 
-# Initialize Session State 
-if 'symbol' not in st.session_state:
-    st.session_state['symbol'] = ''
-if 'retry_count' not in st.session_state:
-    st.session_state['retry_count'] = 0
+# URL mode
+try:
+    mode = int(st.query_params.get("mode", 0))
+except:
+    mode = 0
 
-conn = connect_db()
-if conn is None:
-    st.error("Ah!!! Seems database is playing hide n seek! Try refreshing...")
-    st.stop()      
-
-
-def streamlit_inference(inference, trend):
+# Function: Display Inference Results in Streamlit
+def streamlit_inference(symbol, inference, trend):
     inference = pd.DataFrame.from_dict(inference, orient="index").reset_index()
     column_names = ["Next", "Close"]
     inference.columns = column_names
 
+    st.markdown(f"### {symbol} Analysis...")
     st.success("For the next 7 days, the stock price forcast is:")
     st.dataframe(inference, use_container_width=True)
 
@@ -141,82 +123,101 @@ def streamlit_inference(inference, trend):
     # Display final analysis
     st.markdown(analysis_message)
 
+# Title Section
+st.title("ProInvest — ahead of world!")
+st.caption("Your stock assistant for optimizing investment strategies.")
+st.markdown(
+    f"""
+    <div style="position: fixed; bottom: 5px; right: 10px; font-size: 12px; color: gray;">
+        Version {latest_version}
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# Initialize database connection
+conn = connect_db()
+if conn is None:
+    st.error("Ah!!! Seems database is playing hide n seek! Try refreshing...")
+    st.stop()  
+
 # Main Mode
 if mode == 0:
-    st.markdown("### Search for your stock")
-    user_input = st.text_input("Drop a stock symbol (e.g., TCS, INFY, TSLA)", value=st.session_state.get('symbol', ''))
-    exchange = st.selectbox("Select Exchange", ["NSE", "BSE", "USA"])
+    symbol = st.session_state.get('symbol', '')
+    retry_count = st.session_state.get('retry_count', -1)
 
-    if exchange == "NSE":
-        symbol = f"{user_input}.NS"
-    elif exchange == "BSE":
-        symbol = f"{user_input}.BO"
-    else:
-        symbol = f"{user_input}"
-    
-    if st.button("Smash it!") or st.session_state['retry_count'] > 0:
-        if not user_input.strip():
-            st.warning("Please enter a valid stock symbol before searching.")
-            st.stop()
+    if not symbol and retry_count == -1:
+        st.markdown("### Search for your stock")
+        user_input = st.text_input("Drop a stock symbol (e.g., TCS, INFY, TSLA)").strip().upper()
+        exchange = st.selectbox("Select Exchange", ["NSE", "BSE", "USA"])
 
-        symbol = symbol.upper()
-
-        st.session_state['symbol'] = symbol
-
-        st.spinner(f"Looking up `{symbol}`...")
-
-        if not check_stock_in_list(conn, symbol):
-
-            st.spinner(f"Looking up `{symbol}`... might take a moment...")
-
-            st.session_state['retry_count'] += 1
-
-            if st.session_state['retry_count'] > 10:
-                st.error("Seems market is busy digging gold. Please try later!")
+        if st.button("Smash it!"): 
+            if not user_input:
+                st.warning("Please enter a valid stock symbol before searching.")
                 st.stop()
+        
+            symbol = f"{user_input}.NS" if exchange == "NSE" else f"{user_input}.BO" if exchange == "BSE" else user_input
+            st.session_state.update({'symbol': symbol, 'retry_count': 0})
+            st.rerun()
 
-            with st.spinner(f"Checking the vaults for {symbol}..."):
+    while 0 <= st.session_state['retry_count'] <= MAX_RETRIES:
+        if not check_stock_in_list(conn, symbol):
+            with st.spinner(f"Checking the vaults for {symbol}... might take some seconds..."):
                 try:
                     data_pipeline(conn, symbol)
                 except Exception as e:
                     error_message = str(e)
-
                     if "429" in error_message or "Too Many Requests" in error_message:
+                        st.session_state['retry_count'] += 1
                         wait_time = random.uniform(2, 4)
                         time.sleep(wait_time)
                         logging.info(f"Waiting {wait_time:.2f} seconds before retrying...")
-                        
-                        if st.session_state['retry_count'] <= 10:
-                            st.rerun()  
-                        else:
-                            st.error("Retries exceeded. Please try again later!")
-                            st.stop()
+                        st.rerun()  
                     else:
                         logging.info(e)
                         st.error("Server down! Please try later!")
                         st.stop()
-
+        
         inference, trend = fetch_inference_result(conn, symbol)
         if inference:
-            streamlit_inference(inference, trend)
+            streamlit_inference(symbol, inference, trend)
         else:
             st.error("🚨 Weird! Data exists, but no predictions found.")
+        break
+    if st.session_state['retry_count'] > MAX_RETRIES:
+        st.error("Seems market is busy digging gold. Please try later!")
 
     if st.button("Refresh!"):
-        st.session_state.pop("symbol", None) 
-        st.session_state['retry_count'] = 0  
+        st.session_state.update({'symbol': '', 'retry_count': -1})
         st.rerun()
 
 # Daily update (Admin mode)
 elif mode == 772001:
     st.markdown("### Daily Update Trigger (Admin Mode)")
-    if st.button("Update!"):
+    tooManyRequestFlag = False
+
+    while st.session_state['retry_count'] < 0:
         with st.spinner("Initiating backend wizardry... might take a moment"):
             try:
+                st.session_state['retry_count'] += 1
                 daily_update(conn)
-                st.success("Stocks updated! We’re fresh as morning chai")
             except Exception as e:
-                st.error(f"Daily update failed: {str(e)}")
+                tooManyRequestFlag = True
+                error_message = str(e)
+                if "429" in error_message or "Too Many Requests" in error_message:
+                    wait_time = random.uniform(2, 4)
+                    time.sleep(wait_time)
+                    logging.info(f"Waiting {wait_time:.2f} seconds before retrying...")
+                    st.rerun()  
+    else:
+        st.error("Daily update failed. Please check log...")
+        st.stop()
+
+    if not tooManyRequestFlag: 
+        st.success("Stocks updated! We’re fresh as morning chai")
+
+    if st.button("Refresh!"):
+        st.rerun()
 
 else:
     st.error("Mode unknown. Are you from the future?")
@@ -224,3 +225,7 @@ else:
 
 if conn:
     conn.close()
+
+st.markdown("---")
+
+
